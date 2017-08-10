d245379bd74d1f6ae6a7ce0d1fde46cd53d4969d
Krzysztof Chomski <kchomski@users.noreply.github.com>
2017-08-10 17:17:40 +0200

    Added unittests for PipManager cls.

--- tests/test_PipManager.py
+++ tests/test_PipManager.py
@@ -1,28 +1,145 @@
 # -*- coding: utf-8 -*-
-import curses
-import sys
-from pip_manager import __version__
-from pip_manager import gui
-from pip_manager.gui import Gui
+from pip_manager import app
+
+from pip_manager.app import PipManager
 
 import pytest
 
 
-@pytest.fixture
-def fake_instance(mocker):
-    _fake_instance = mocker.patch('pip_manager.app.PipManager', autospec=True)
-    _fake_instance.stdscr.getmaxyx.return_value = [24, 80]
-    # _fake_instance.line_width = 79
-    # _fake_instance.menu_height = 12
-    # _fake_instance.get_pages_count.return_value = 5
-    return _fake_instance
+class Test_PipManager(object):
 
+    @pytest.fixture(autouse=True)
+    def custom_init(self, mocker):
+        mocker.spy(app, 'Distribution')
+        mocker.patch('pip_manager.distribution.Distribution.get_newest_version').return_value = '9.99.999'
+        mocker.patch('pip_manager.distribution.pip', autospec=True)
 
-@pytest.fixture
-def mocked_curses(mocker):
-    _mocked_curses = mocker.patch('pip_manager.app.curses')
-    return _mocked_curses
+        self.mocked_pip = mocker.patch('pip_manager.app.pip', autospec=True)
 
+        class FakePipDist(object):
+            def __init__(self, key, version):
+                self.key = key
+                self.version = version
 
-def test_dist_win_height(fake_instance):
-    assert hasattr(fake_instance, 'dist_win_height')
+        self.mocked_pip.main.return_value = None
+        self.mocked_pip.get_installed_distributions.return_value = [
+            FakePipDist(key, version) for key, version in [
+                ['alabaster', '0.7.10'],
+                ['Babel', '2.4.0'],
+                ['bleach', '2.0.0'],
+                ['certifi', '2017.7.27.1'],
+                ['chardet', '3.0.4'],
+                ['decorator', '4.1.2'],
+                ['Django', '1.11.4'],
+                ['docutils', '0.14'],
+                ['entrypoints', '0.2.3'],
+                ['html5lib', '0.999999999'],
+                ['idna', '2.6'],
+                ['imagesize', '0.7.1'],
+                ['ipykernel', '4.6.1'],
+                ['ipyparallel', '6.0.2'],
+                ['ipython', '6.1.0'],
+                ['ipython-genutils', '0.2.0'],
+                ['ipywidgets', '6.0.0'],
+                ['itsdangerous', '0.24'],
+                ['jedi', '0.10.2'],
+                ['Jinja2', '2.9.6'],
+                ['jsonschema', '2.6.0'],
+                ['jupyter-client', '5.1.0'],
+                ['jupyter-core', '4.3.0'],
+                ['MarkupSafe', '1.0'],
+                ['mistune', '0.7.4'],
+                ['nbconvert', '5.2.1'],
+                ['nbformat', '4.3.0'],
+                ['nose', '1.3.7'],
+                ['notebook', '5.0.0'],
+                ['pandocfilters', '1.4.2'],
+                ['pexpect', '4.2.1'],
+                ['pickleshare', '0.7.4'],
+                ['pip', '9.0.1'],
+                ['prompt-toolkit', '1.0.15'],
+                ['ptyprocess', '0.5.2'],
+                ['py', '1.4.34'],
+                ['Pygments', '2.2.0'],
+                ['pytest', '3.2.1'],
+                ['pytest-mock', '1.6.2'],
+                ['pytest-runner', '2.11.1'],
+                ['python-dateutil', '2.6.1'],
+                ['pytz', '2017.2'],
+                ['pyzmq', '16.0.2'],
+                ['qtconsole', '4.3.0'],
+                ['requests', '2.18.3'],
+                ['setuptools', '36.2.7'],
+                ['simplegeneric', '0.8.1'],
+                ['six', '1.10.0'],
+                ['snowballstemmer', '1.2.1'],
+                ['Sphinx', '1.6.3'],
+                ['sphinxcontrib-websupport', '1.0.1'],
+                ['terminado', '0.6'],
+                ['testpath', '0.3.1'],
+                ['tornado', '4.5.1'],
+                ['traitlets', '4.3.2'],
+                ['urllib3', '1.22'],
+                ['wcwidth', '0.1.7'],
+                ['webencodings', '0.5.1'],
+                ['Werkzeug', '0.12.2'],
+                ['wheel', '0.29.0'],
+                ['widgetsnbextension', '2.0.0'],
+                ['yapf', '0.16.3']
+            ]
+        ]
+
+        self.mocked_gui = mocker.patch('pip_manager.app.Gui', autospec=True)
+        mocker.spy(PipManager, 'get_distributions')
+        self.pm = PipManager()
+
+    def test_initialization(self):
+        self.mocked_gui.assert_called_with(79)
+        assert self.pm.page == 0
+        assert self.pm.cursor_pos == 0
+        assert self.pm.get_distributions.called
+
+    @pytest.mark.parametrize(
+        'page, dist_win_height, start_idx, stop_idx',
+        [
+            (0, 17, 0, 17),
+            (0, 20, 0, 20),
+            (1, 20, 20, 40),
+            (0, 100, 0, 100),
+            (0, 100, 0, 80),
+        ]
+
+    )
+    def test_dists_to_draw(self, page, dist_win_height, start_idx, stop_idx):
+        self.pm.page = page
+        self.pm.gui.dist_win_height = dist_win_height
+        assert self.pm.dists_to_draw == self.pm.distributions[start_idx:stop_idx]
+
+    def test_get_distributions(self):
+        rv = self.pm.get_distributions()
+        assert self.mocked_pip.get_installed_distributions.called
+        assert self.pm.gui.draw_popup.called
+        assert app.Distribution.called
+        assert self.pm.distributions == rv
+
+    def test_toggle_one(self):
+        self.pm.toggle_one(0)
+        assert self.pm.distributions[0].is_selected
+        self.pm.toggle_one(0)
+        assert not self.pm.distributions[0].is_selected
+
+    def test_toggle_all(self):
+        self.pm.toggle_all()
+        assert all(d.is_selected for d in self.pm.distributions)
+        self.pm.toggle_all()
+        assert not any(d.is_selected for d in self.pm.distributions)
+
+    def test_update_distributions(self):
+        for i in range(5):
+            self.pm.distributions[i].is_selected = True
+
+        self.pm.update_distributions()
+        assert self.pm.gui.draw_popup.called
+
+        for d in self.pm.distributions[:5]:
+            self.mocked_pip.main.assert_any_call(['install', '--upgrade', d.name])
